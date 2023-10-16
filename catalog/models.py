ea4fc1833c56034d7dec653783f0f2b4f12e59a4
Tuyana Chirninova <hikkonki@gmail.com>
2023-10-16 20:05:28 +0300

    catalog app files added (init, admin, apps, models, tests)

---
+++ catalog/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+
+from config import settings
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    photo = models.ImageField(upload_to='products/', **NULLABLE, verbose_name='Изображение(превью)')
+    category = models.CharField(max_length=100, verbose_name='Категория')
+    price = models.FloatField(verbose_name='Цена')
+    date_created = models.DateField(verbose_name='Дата создания')
+    date_last_modified = models.DateField(verbose_name='Дата последнего изменения')
+
+    auth_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Авторизованный пользователь')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+class Version(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукт')
+    number = models.IntegerField(verbose_name='Номер версии')
+    name = models.CharField(max_length=150, verbose_name='Название версии')
+    is_active = models.BooleanField(default=True, verbose_name='Текущая версия')
+
+    def __str__(self):
+        return f'{self.product} версия {self.number} {self.name}'
+
+    class Meta:
+        verbose_name = 'версия'
+        verbose_name_plural = 'версии'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class BlogEntry(models.Model):
+    heading = models.CharField(max_length=250, verbose_name='Заголовок')
+    slug = models.CharField(max_length=150, verbose_name='slug', null=True, blank=True)
+    content = models.TextField(verbose_name='Содержимое')
+    image = models.ImageField(upload_to='blog/', ** NULLABLE, verbose_name='Изображение(превью)')
+    date_created = models.DateField(** NULLABLE, verbose_name='Дата создания')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    views_count = models.IntegerField(default=0, verbose_name='Количество просмотров')
+
+    def __str__(self):
+        return self.heading
+
+    class Meta:
+        verbose_name = 'запись блога'
+        verbose_name_plural = 'записи блога'
