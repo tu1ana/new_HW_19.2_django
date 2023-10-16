1ca095c68c1d59045b2d7b82df4431c2dde1327c
Tuyana Chirninova <hikkonki@gmail.com>
2023-10-16 20:12:50 +0300

    users app package

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from catalog.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    phone = models.CharField(max_length=35, verbose_name='Номер телефона', **NULLABLE)
+    country = models.CharField(max_length=250, verbose_name='Страна', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
