5c17e668713c2a318f4db969f40f52da90d7a601
Tuyana Chirninova <hikkonki@gmail.com>
2023-10-17 19:55:56 +0300

    register decorator for Category, Product, Version models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product, Version
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
+
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('number', 'name',)
+    list_filter = ('is_active',)
