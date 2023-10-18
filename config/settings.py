025459a3e9aae0341cf82ede74e5f8a1df696a54
Tuyana Chirninova <hikkonki@gmail.com>
2023-10-18 15:42:30 +0300

    LOGIN_URL added

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,8 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+LOGIN_URL = '/users/'
+
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'fuckup@oscarbot.ru'
